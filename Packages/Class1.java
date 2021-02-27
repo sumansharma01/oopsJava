3ef2abd04e93c6635e0a0a66cd9b38926d9c6e66
sumansharma01 <suman.sharma0331@gmail.com>
2021-02-27 11:38:14 +0530

    added packages in ide

---
+++ Packages/Class1.java
@@ -0,0 +1,11 @@
+package Packages;
+
+public class Class1 {
+    public static void display(String a,String b){
+        System.out.println(a+b);
+    }
+
+    public static void display2(){
+        System.out.println("Hello second");
+    }
+}
---
+++ Packages2/Main.java
@@ -0,0 +1,7 @@
+package Packages2;
+import Packages.Class1;
+public class Main {
+    public static void main(String[] args) {
+        Class1.display("hello","Suman");
+    }
+}
